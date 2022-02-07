b8243b43e7cb8ceea8c587c0fd82615880b5edea
Anthony Ulloa <aulloan93@gmail.com>
2022-02-07 01:58:35 -0500

    part3 - phonebook backend - step5

--- index.js
+++ index.js
@@ -59,6 +59,30 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body || !body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  const newPerson = {
+    id: generateId(),
+    name: body.name,
+    number: body.number || ''
+  }
+
+  persons = persons.concat(newPerson)
+
+  response.json(newPerson)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
